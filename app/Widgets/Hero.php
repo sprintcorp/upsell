7c673af11194172eb9ec83c506ae68a2c3b532f8
Frederick Adebayo <sprintcorp7@gmail.com>
2022-07-07 15:54:38 +0100

    project initialization

---
+++ app/Widgets/Hero.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Widgets;
+
+use Arrilot\Widgets\AbstractWidget;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use TCG\Voyager\Facades\Voyager;
+
+class Hero extends AbstractWidget
+{
+    /**
+     * The configuration array.
+     *
+     * @var array
+     */
+    protected $config = [];
+
+    /**
+     * Treat this method as a controller action.
+     * Return view() or other content to display.
+     */
+    public function run()
+    {
+        $count = \App\Models\Hero::count();
+        $string = 'Hero';
+
+        return view('voyager::dimmer', array_merge($this->config, [
+            'icon'   => 'voyager-medal-rank-star',
+            'title'  => "{$count} {$string}",
+//            'text'   => __('voyager::dimmer.post_text', ['count' => $count, 'string' => Str::lower($string)]),
+            'button' => [
+                'text' =>'Hero',
+                'link' => route('voyager.heroes.index'),
+            ],
+            'image' => voyager_asset('images/widget-backgrounds/02.jpg'),
+        ]));
+    }
+
+    /**
+     * Determine if the widget should be displayed.
+     *
+     * @return bool
+     */
+    public function shouldBeDisplayed()
+    {
+        return Auth::user()->can('browse', '\App\Models\Hero');
+    }
+}
